b790c066cb751437f6ae09c0653e7a288ef4edf4
Avadhut <avadhut98@hotmail.com>
2024-01-20 12:23:31 +0530

    Add files via upload

---
+++ src/com/greatlearning/CurrencyDenomination/main.java
@@ -0,0 +1,39 @@
+package com.greatlearning.CurrencyDenomination;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//import com.greatlearning.services.MergeSort;
+//import com.greatlearning.services.NotesCount;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Initialize Scanner
+		Scanner scanner = new Scanner(System.in);
+		//Initialize MergeSort and NotesCount objects
+		MergeSort mergeSort = new MergeSort();
+		NotesCount notesCount = new NotesCount();
+		System.out.println("Enter the size of currency denominations");
+		int totalNoOfDenominations = scanner.nextInt();
+		//Initialize the notes array
+		int[] notes = new int[totalNoOfDenominations];
+		//Get currency denominations value
+		System.out.println("Enter currency Denominations value");
+		for(int i=0; i<totalNoOfDenominations; i++) {
+			notes[i] = scanner.nextInt();
+		}
+		//Get the amount to pay
+		System.out.println("Enter the amount you want to pay");
+		int amount = scanner.nextInt();
+		//Merge Sort
+		mergeSort.sort(notes, 0, notes.length-1);
+		//countNotes
+		//System.out.println(Arrays.toString(notes));
+		notesCount.notesCountImplementation(notes,  amount);
+		
+		scanner.close();
+	}
+
+}
---
+++ src/com/greatlearning/paymoney/main.java
@@ -0,0 +1,48 @@
+package com.greatlearning.paymoney;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//import com.greatlearning.services.TransactionTargetAlgo;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		TransactionTargetAlgo transactionTargetAlgo = new TransactionTargetAlgo();
+        System.out.println("Enter the size of transaction array");
+        //Get totalNoOfTransactions of the transactions array
+        int totalNoOfTransactions = scanner.nextInt();
+        //Initialize the transactions array
+        int[] transactions = new int[totalNoOfTransactions];
+        		
+        System.out.println("Enter the values of array");
+        //Get the transactions
+        for(int i=0; i<totalNoOfTransactions; i++)
+        	transactions[i] = scanner.nextInt();
+        //Get the no. of targets
+        System.out.println("Enter the total no. of targets that needs to be achieved");
+        int targetCount = scanner.nextInt();
+        System.out.println("Enter the value of targets");
+        int target;
+        //[TBD]Calculate the min and overall sum in advance and check
+        // Input:  arr[] = {10, 20, 10, 5, 15}
+        // Output: prefixSum[] = {10, 30, 40, 45, 60}
+        int prefixSum[] = new int[totalNoOfTransactions];
+        prefixSum[0] = transactions[0];
+        for(int i=1; i<totalNoOfTransactions; i++) {
+        	prefixSum[i] = prefixSum[i-1] + transactions[i];
+        }
+        //System.out.println(Arrays.toString(prefixSum));
+        for(int i=0; i<targetCount; i++) {
+        	target = scanner.nextInt();
+        	transactionTargetAlgo.processTransactions(prefixSum, target);
+        }
+        
+        scanner.close();
+
+	
+
+	}
+
+}
